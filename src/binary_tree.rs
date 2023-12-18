80401265b38d0d63e1af8a6ae9f7a2cd915b07c4
Jakub Stacherski <o503@interia.eu>
2023-12-19 00:20:22 +0100

    Creating example tree; Printing all paths to leaves

--- src/binary_tree.rs
+++ src/binary_tree.rs
@@ -1,25 +1,56 @@
+#[derive(Clone)]
 pub struct Node<'a> {
-    pub left_child: Option<&'a Node<'a>>,
-    pub right_child: Option<&'a Node<'a>>,
-    pub value: &'a str,
+    pub text: &'a str,
+    pub left_child: Option<Box<Node<'a>>>,
+    pub right_child: Option<Box<Node<'a>>>,
 }
 
 impl<'a> Node<'a> {
-    pub fn new(value: &'a str) -> Self {
+    pub fn new(
+        value: &'a str,
+        left_child: Option<Box<Node<'a>>>,
+        right_child: Option<Box<Node<'a>>>,
+    ) -> Self {
         Self {
-            value,
-            left_child: None,
-            right_child: None,
+            text: value,
+            left_child,
+            right_child,
         }
     }
-}
 
-fn print_tree_paths_rec(node: Option<&Node>, path_text: &str) {
-    if let None = node {
-        return;
+    fn pack(node: Self) -> Option<Box<Node<'a>>> {
+        Some(Box::new(node))
     }
 }
 
-pub fn print_tree_paths(root: &Node) {
-    print_tree_paths_rec(Some(root), "");
+fn print_tree_paths_rec(node: &Option<Box<Node>>, path_text: String) {
+    if let Some(node) = node {
+        let path_text = format!("{}{path_text}", node.text);
+
+        if node.left_child.is_none() && node.right_child.is_none() {
+            println!("{path_text}");
+            return;
+        }
+
+        print_tree_paths_rec(&node.left_child, path_text.clone());
+        print_tree_paths_rec(&node.right_child, path_text.clone());
+    }
+}
+
+pub fn print_tree_paths(root: Node) {
+    let root = &Some(Box::new(root));
+    print_tree_paths_rec(root, "".to_owned());
+}
+
+pub fn get_example_tree<'a>() -> Node<'a> {
+    let left_branch = Node::new("google", None, None);
+
+    let right_branch = Node::new("studia", None, None);
+    let right_child = Node::new(".pw", Node::pack(right_branch), None);
+    let right_child = Node::new(".edu", None, Node::pack(right_child));
+
+    let root = Node::new(".pl", Node::pack(left_branch), Node::pack(right_child));
+    let root = Node::new(".", Node::pack(root), None);
+
+    root
 }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ mod binary_tree;
 use binary_tree::*;
 
 fn main() {
-    let mut root = Node::new(".");
-    root.left_child = Some(&Node::new(".pl"));
-
-    print_tree_paths(&root);
+    let root = get_example_tree();
+    print_tree_paths(root.clone());
 }
