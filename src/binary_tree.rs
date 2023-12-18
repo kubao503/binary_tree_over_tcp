41b24048de89115e631cc283d979bef7f5b560c4
Jakub Stacherski <o503@interia.eu>
2023-12-19 00:31:09 +0100

    NodeChild type alias

--- src/binary_tree.rs
+++ src/binary_tree.rs
@@ -1,16 +1,14 @@
 #[derive(Clone)]
 pub struct Node<'a> {
     pub text: &'a str,
-    pub left_child: Option<Box<Node<'a>>>,
-    pub right_child: Option<Box<Node<'a>>>,
+    pub left_child: NodeChild<'a>,
+    pub right_child: NodeChild<'a>,
 }
 
+type NodeChild<'a> = Option<Box<Node<'a>>>;
+
 impl<'a> Node<'a> {
-    pub fn new(
-        value: &'a str,
-        left_child: Option<Box<Node<'a>>>,
-        right_child: Option<Box<Node<'a>>>,
-    ) -> Self {
+    pub fn new(value: &'a str, left_child: NodeChild<'a>, right_child: NodeChild<'a>) -> Self {
         Self {
             text: value,
             left_child,
@@ -18,12 +16,12 @@ impl<'a> Node<'a> {
         }
     }
 
-    fn pack(node: Self) -> Option<Box<Node<'a>>> {
+    fn new_child(node: Self) -> NodeChild<'a> {
         Some(Box::new(node))
     }
 }
 
-fn print_tree_paths_rec(node: &Option<Box<Node>>, path_text: String) {
+fn print_tree_paths_rec(node: &NodeChild, path_text: String) {
     if let Some(node) = node {
         let path_text = format!("{}{path_text}", node.text);
 
@@ -33,12 +31,12 @@ fn print_tree_paths_rec(node: &Option<Box<Node>>, path_text: String) {
         }
 
         print_tree_paths_rec(&node.left_child, path_text.clone());
-        print_tree_paths_rec(&node.right_child, path_text.clone());
+        print_tree_paths_rec(&node.right_child, path_text);
     }
 }
 
 pub fn print_tree_paths(root: Node) {
-    let root = &Some(Box::new(root));
+    let root = &Node::new_child(root);
     print_tree_paths_rec(root, "".to_owned());
 }
 
@@ -46,11 +44,15 @@ pub fn get_example_tree<'a>() -> Node<'a> {
     let left_branch = Node::new("google", None, None);
 
     let right_branch = Node::new("studia", None, None);
-    let right_child = Node::new(".pw", Node::pack(right_branch), None);
-    let right_child = Node::new(".edu", None, Node::pack(right_child));
+    let right_child = Node::new(".pw", Node::new_child(right_branch), None);
+    let right_child = Node::new(".edu", None, Node::new_child(right_child));
 
-    let root = Node::new(".pl", Node::pack(left_branch), Node::pack(right_child));
-    let root = Node::new(".", Node::pack(root), None);
+    let root = Node::new(
+        ".pl",
+        Node::new_child(left_branch),
+        Node::new_child(right_child),
+    );
+    let root = Node::new(".", Node::new_child(root), None);
 
     root
 }
